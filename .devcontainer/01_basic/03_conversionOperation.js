42077ff13e87b68b276e6406e85e68f2653e12da
Soumen Mondal <123764221+Souspy@users.noreply.github.com>
2024-08-02 18:04:21 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basic/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score="33abc"
+// console.log(typeof score)
+
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber))  //when we have to take any value in number format just use Number().....
+//console.log(typeof(score))
+//console.log(valueInNumber) // it gives output as Nan(nit anumber)because abc can't be convert into number..
+//console.log(score)
+//"33"=>33
+//"33abc"=>Nan
+// true =>1; false =>0
+
+let isLoggedIn=""
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+// 1=> true; 0 =>false
+// " "=>false
+// "Soum"=>true
+
+let somenumber=12
+let stringsome=String(somenumber)
+console.log(stringsome)
+console.log(typeof(stringsome))
