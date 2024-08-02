e0f35ea8b2b4f9d615277a0e968e8b0f7adbf95f
Soumen Mondal <123764221+Souspy@users.noreply.github.com>
2024-08-02 11:52:08 +0000

    datatypes

---
+++ .devcontainer/01_basic/02_datatypes.js
@@ -0,0 +1,28 @@
+"Use strict";  //treat all js code as a newer version
+
+//alert(3+3)  //we are using node js not browser babe so you can't do this!!
+
+console.log(3
+    + 3)    //kuch to sharam karo aise code kau likhte hai thoda dhank se likho code ka redeability kharab nhi hona chaye... prettier har kaam nhi samhalta kuch kuch khud se samhalna padta hai....
+console.log("Sou")
+
+let name="Sou"
+let age=22
+let isLogedIn= false
+let state;
+
+// chalo kya kya datatypes hota hai dekh lete hain..!!
+//premetive datatypes----
+//number => 2 to the power 53.. ye limit hai boss!!
+//bigint
+//string=>"double cot ke andar hi use kar lena warna dikkat ho sakta hai!!"
+//boolean => true/false
+//null=> this is a standalone value alongwith a datatype
+//undefined => value not assigned yet.
+//symbol => to find uniqueness
+
+//object
+
+console.log(typeof "Sou")
+console.log(typeof null)
+console.log(typeof undefined)
